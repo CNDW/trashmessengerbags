1091280846877f710d46e18f76628381000b4c05
Travis Peterson <tekman82@yahoo.com>
2013-11-18 17:46:14 -0600

    Initialize Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trashmessengerbags::Application.config.secret_key_base = '6f9a1871f4cb7d29ce6ec4f7368207735738c97fec9a8ab40e7907234026b83ef374ab0ae0e0df92bb8d9940f4b7c8dcd7392c2aaf1ccf017cd55e6cb361260e'
