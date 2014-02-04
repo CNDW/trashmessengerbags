4ccb1d1992f93cfce44b13666ef1b6b43a089ada
Travis Peterson <tekman82@yahoo.com>
2014-02-04 12:35:59 -0600

    Adds random seed data for development database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
 Admin.create!(name: 'admin', password: 'password', password_confirmation: 'password', email: 'admin@trashbags.com')
 
+categorization = ['Bags', 'Apparel', 'Utility']
+16.times do 
+	ProductCategory.create(name: Faker::Commerce.department, desc: Faker::Lorem.sentence, category: categorization.sample)
+end
+
+30.times do
+	Product.create(name: Faker::Commerce.product_name, desc: Faker::Lorem.sentence, price: Random.rand(100), product_category: ProductCategory.all.sample)
+end
+
+15.times do
+	ProductOption.create(name: Faker::Commerce.color, desc: Faker::Lorem.sentence, price: Random.rand(50), products: Product.all.sample(6))
+end
